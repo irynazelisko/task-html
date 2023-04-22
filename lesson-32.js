5e6ada7640723693b6ac7f66b1e342bcce318982
Iryna <irazelisko@gmail.com>
2023-04-22 22:39:17 +0300

    lesson-34

--- lesson-32.js
+++ lesson-32.js
@@ -1,15 +1,33 @@
 
+class GithubApi {
+  constructor(token, username) {
+    this.token = token;
+    this.username = username;
+  };
+
+  getRepos() {
+    let headers = {
+      'Authorization': 'Bearer ' + `${this.token}`
+    };
+
+    return fetch(`https://api.github.com/users/${this.username}/repos`, { headers })
+      .then(response => response.json())
+  };
+};
+
+// Створення нового екземпляру класу та використання методу getRepos()
+const api = new GithubApi('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', 'irynazelisko');
+
 const reposList = document.getElementById("repositories-list");
 
-fetch(`https://api.github.com/users/irynazelisko/repos`)
-  .then((response) => response.json())
+api.getRepos()
   .then((repos) => {
     repos.forEach((repo) => {
       const eachRepo = document.createElement("div");
       eachRepo.style.border = '1px solid black';
       eachRepo.style.padding = '10px';
       eachRepo.style.marginBottom = '15px';
-     
+
       const repoLink = document.createElement("a");
       repoLink.textContent = repo.full_name;
       repoLink.href = repo.html_url;
@@ -26,10 +44,3 @@ fetch(`https://api.github.com/users/irynazelisko/repos`)
       reposList.appendChild(eachRepo);
     });
   });
- 
-
-       
-
-
-
-
