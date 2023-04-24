8178f4903688b01d4f959ed7f6312ea702a4863d
Iryna <irazelisko@gmail.com>
2023-04-24 20:43:35 +0300

    add new token for teacher check

--- lesson-32.js
+++ lesson-32.js
@@ -16,7 +16,7 @@ class GithubApi {
 };
 
 // Створення нового екземпляру класу та використання методу getRepos()
-const api = new GithubApi('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', 'irynazelisko');
+// const api = new GithubApi('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', 'irynazelisko');
 
 const reposList = document.getElementById("repositories-list");
 
